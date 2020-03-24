a012052013014478e30bb248753002bd26ba50f4
Pedro Henrique <pedrorodriguesfereira@gmail.com>
2020-03-24 17:06:30 -0300

    Criação de getters e setters para classe Pessoa

---
+++ Main.dart
@@ -0,0 +1,27 @@
+void main()
+{
+
+
+}
+
+class Pessoa
+{
+
+   String _nome;
+  double _altura;
+  DateTime _niver;
+  
+  
+  set nome(String nome){this._nome = nome;}
+  String get nome{return this._nome;}
+  
+  
+  
+  set altura(double altura){this._altura = altura;}
+  double get altura{return this._altura;}
+  
+  set niver(DateTime niver){this._niver = niver;}
+  DateTime get niver{return this._niver;}
+}
+
+
