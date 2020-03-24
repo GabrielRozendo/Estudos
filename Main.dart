2038dd650064c574dcaa0fc424fab60631a2c9d0
Pedro Henrique <pedrorodriguesfereira@gmail.com>
2020-03-24 17:10:42 -0300

    criação do método de listar dados e adicionado idade automática a pessoa

--- Main.dart
+++ Main.dart
@@ -1,6 +1,10 @@
 void main()
 {
-
+   Pessoa p1 = Pessoa();
+  p1.nome = "Pedro";
+  p1._altura = 1.76;
+  p1._niver =  DateTime.parse("2003-06-12");
+ print(p1.dados());
 
 }
 
@@ -15,13 +19,18 @@ class Pessoa
   set nome(String nome){this._nome = nome;}
   String get nome{return this._nome;}
   
-  
+   int idade(){return DateTime.now().year- niver.year;}
   
   set altura(double altura){this._altura = altura;}
   double get altura{return this._altura;}
   
   set niver(DateTime niver){this._niver = niver;}
   DateTime get niver{return this._niver;}
-}
+
+  String dados()
+  {
+    
+    return "Nome: "+nome+"\nIdade: "+idade().toString()+"\nAltura: "+altura.toString()+"\nNiver: "+niver.toString();
+  }
 
 
