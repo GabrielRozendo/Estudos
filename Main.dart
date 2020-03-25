3678cadbe4e25f65525e0b05517489ed9a8a5bed
Pedro Henrique <pedrorodriguesfereira@gmail.com>
2020-03-25 17:46:51 -0300

    Adição de construtor, nome maiúsculo e correção  do cálculo de idade

--- Main.dart
+++ Main.dart
@@ -1,35 +1,40 @@
 void main()
 {
-  Pessoa p1 = Pessoa();
-  p1.nome = "Pedro";
-  p1._altura = 1.76;
-  p1._niver = DateTime.parse("2003-06-12");
-  print(p1.dados());
+    Pessoa p1 = Pessoa("Pedro", 1.76, DateTime.parse("2003-06-12"));
+    print(p1.dados());
 
 }
 
 class Pessoa {
 
-  String _nome;
-  double _altura;
-  DateTime _niver;
+    String _nome;
+    double _altura;
+    DateTime _niver;
 
+    Pessoa(String nome, double altura, DateTime niver) {
+        this._nome = nome;
+        this._altura = altura;
+        this._niver = niver;
 
-  set nome(String nome) { this._nome = nome; }
-  String get nome{ return this._nome; }
+    }
 
-  int idade() { return DateTime.now().year - niver.year; }
+    String get nome{ return this._nome.toUpperCase(); }
+    double get altura{ return this._altura; }
+    DateTime get niver{ return this._niver; }
 
-  set altura(double altura) { this._altura = altura; }
-  double get altura{ return this._altura; }
+    int idade() {
 
-  set niver(DateTime niver) { this._niver = niver; }
-  DateTime get niver{ return this._niver; }
+        if (DateTime.now().month - niver.month == 0) {
 
-  String dados() {
+            return (DateTime.now().year - niver.year);
+        }
 
-     "Nome:$nome \nIdade:${idade().toString()} \nAltura:${altura.toString()} \nNiver:${niver.toString()}";
-  }
+        else { return (DateTime.now().year - niver.year) - 1; }
+    }
 
+    String dados() {
 
+        return "Nome:$nome\nIdade:${idade().toString()}\nAltura:${altura.toString()}\nNiver:${niver.toString()}";
+    }
 
+}
