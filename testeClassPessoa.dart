a545fe8832d1207a62526b9a245c7d86f2b86c74
Pedro Henrique <pedrorodriguesfereira@gmail.com>
2020-03-25 16:21:08 -0300

    Criação do esqueleto da classe Pessoa

---
+++ testeClassPessoa.dart
@@ -0,0 +1,26 @@
+void main()
+{
+
+}
+
+
+abstract class Pessoa
+{
+  
+  String _nome;
+  int _idade;
+  String _cpf;
+  
+  set nome(String nome){this._nome = nome;}
+  get nome => this._nome;
+  
+  set idade(int idade){this._idade = idade;}
+  get idade => this._idade;
+  
+  set cpf(String cpf){this._cpf = cpf;}
+  get cpf => this._cpf;
+  
+ 
+  
+  
+}
