914b60a7e64abd8198a14897519506b4e6d371d2
Pedro Henrique <pedrorodriguesfereira@gmail.com>
2020-03-25 17:19:05 -0300

    Código identado e interpolação de String adicionada

--- Main.dart
+++ Main.dart
@@ -1,36 +1,35 @@
 void main()
 {
-   Pessoa p1 = Pessoa();
+  Pessoa p1 = Pessoa();
   p1.nome = "Pedro";
   p1._altura = 1.76;
-  p1._niver =  DateTime.parse("2003-06-12");
- print(p1.dados());
+  p1._niver = DateTime.parse("2003-06-12");
+  print(p1.dados());
 
 }
 
-class Pessoa
-{
+class Pessoa {
 
-   String _nome;
+  String _nome;
   double _altura;
   DateTime _niver;
-  
-  
-  set nome(String nome){this._nome = nome;}
-  String get nome{return this._nome;}
-  
-   int idade(){return DateTime.now().year- niver.year;}
-  
-  set altura(double altura){this._altura = altura;}
-  double get altura{return this._altura;}
-  
-  set niver(DateTime niver){this._niver = niver;}
-  DateTime get niver{return this._niver;}
 
-  String dados()
-  {
-    
-    return "Nome: "+nome+"\nIdade: "+idade().toString()+"\nAltura: "+altura.toString()+"\nNiver: "+niver.toString();
+
+  set nome(String nome) { this._nome = nome; }
+  String get nome{ return this._nome; }
+
+  int idade() { return DateTime.now().year - niver.year; }
+
+  set altura(double altura) { this._altura = altura; }
+  double get altura{ return this._altura; }
+
+  set niver(DateTime niver) { this._niver = niver; }
+  DateTime get niver{ return this._niver; }
+
+  String dados() {
+
+     "Nome:$nome \nIdade:${idade().toString()} \nAltura:${altura.toString()} \nNiver:${niver.toString()}";
   }
+
 
 
