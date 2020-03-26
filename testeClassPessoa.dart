34a55a2a7d10c78776bfdec5a6b3e94720ccfa47
Pedro Henrique <pedrorodriguesfereira@gmail.com>
2020-03-26 12:02:05 -0300

    Melhoria no método _pesoPerdido, mostra em g ou kg

--- testeClassPessoa.dart
+++ testeClassPessoa.dart
@@ -1,79 +1,76 @@
 void main() {
 
-Atleta ok = Atleta();
-  ok.nome = "Pedro";
-  ok.peso = 120;
-  ok.treinar(2000);
- 
+    Atleta ok = Atleta();
+    ok.nome = "Pedro";
+    ok.peso = 76;
+    ok.treinar(9000);
+
 
 }
 
 
-abstract class Pessoa
-{
-  
-  String _nome;
-  int _idade;
-  double _peso;
-  String _cpf;
-  
-  set nome(String nome){this._nome = nome;}
-  get nome => this._nome;
-  
-  set idade(int idade){this._idade = idade;}
-  get idade => this._idade;
-  
-  set peso(double peso){this._peso = peso;}
-  get peso => this._peso.toStringAsPrecision(4);
-  
-  set cpf(String cpf){this._cpf = cpf;}
-  get cpf => this._cpf;
-  
- 
-  
-  
-}
+abstract class Pessoa {
 
+    String _nome;
+    int _idade;
+    double _peso;
+    String _cpf;
 
-class Atleta extends Pessoa
-{
-  String _tipo;
-  int _numMedalhas;
-  
-  set tipo(String tipo){this._tipo = tipo;}
-  get tipo => this._tipo;
+    set nome(String nome) => this._nome = nome;
+    get nome => this._nome;
 
-  set medalhas(int medalhas){this._numMedalhas = medalhas;}
-  get medalhas => this._numMedalhas;
-  
+    set idade(int idade) => this._idade = idade;
+    get idade => this._idade;
 
-  
-   void treinar(double calorias)
-  {
+    set peso(double peso) => this._peso = peso;
+    get peso => this._peso.toStringAsPrecision(4);
+
+    set cpf(String cpf)=> this._cpf = cpf;
+    get cpf => this._cpf;
+      
      
-    print("O atleta $nome treinou e perdeu ${_pesoPerdido(calorias)}kg! E agora esta com ${peso}Kg");
-   
-  }
-  
-  
-String _pesoPerdido(double calorias)
-  {
-  
-    if(calorias == 70000){
-       calorias = 1 ;
-       this._peso -= calorias;
+      
+      
     }
-    
-     else{
-       calorias = (calorias/7000);
-       
-       this._peso -= calorias;
-       
-       
-     }
-    
-   return calorias.toStringAsPrecision(2);  
-  }
-  
-  
+
+
+class Atleta extends Pessoa {
+    String _tipo;
+    int _numMedalhas;
+
+    set tipo(String tipo) { this._tipo = tipo; }
+    get tipo => this._tipo;
+
+    set medalhas(int medalhas){ this._numMedalhas = medalhas; }
+    get medalhas => this._numMedalhas;
+
+
+
+void treinar(double calorias)
+{
+
+    print("O atleta $nome treinou e perdeu ${_pesoPerdido(calorias)}! E agora esta com ${peso}Kg");
+
+}
+
+
+String _pesoPerdido(double calorias)
+{
+
+    calorias /= 7000;
+
+    if (calorias > 1 || calorias == 1) {
+        this._peso -= calorias;
+        return calorias.toStringAsPrecision(2) + "Kg";
+    }
+
+
+    else {
+        this._peso -= calorias;
+        calorias *=100;
+        return calorias.toStringAsPrecision(2) + "g";
+    }
+
+
+}
 }
