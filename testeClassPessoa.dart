bee7d7669b2f894e996d1b69be176ea8edfea8f9
Pedro Henrique <pedrorodriguesfereira@gmail.com>
2020-03-27 14:30:10 -0300

    Correção no planejamento de treino por parte do Técnico, adicionado classe PlanejamentoTreino e correções na classe Time

--- testeClassPessoa.dart
+++ testeClassPessoa.dart
@@ -1,78 +1,41 @@
+// Copyright 2015 the Dart project authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license
+// that can be found in the LICENSE file.
+
 void main() {
+  Atleta ok = Atleta();
+  ok.nome = "Pedro";
+  ok.idade = 16;
+  ok.peso = 76;
+  ok.cpf = "121.692.176-33";
+  ok.assinatura = "pedro";
+  ok.modalidade = "Futsal";
+  ok.salario = 500;
+  ok.comer(9000);
+  ok.treinar(7000);
+  ok.gastarSalario(500);
+  ok.listarDados();
 
-   //EXECUTAR TESTES 1 DE CADA vEZ ---PS: FAZER TODAS AS VALIDAÇÕES ANTES DE TESTAR
-   //organizar melhor as classes e talvez adicionar outras pra deixar tudo mais organizado
+  Tecnico ok2 = Tecnico();
+  ok2.nome = "Ricardo";
+  ok2.idade = 46;
+  ok2.peso = 80;
+  ok2.cpf = "111.111.111-11";
+  ok2.salario = 1000;
+  ok2.assinatura = "teste";
+  ok2.planejarTreino("Teste", DateTime.parse("2001-12-06"));
 
+  Diretor ok3 = Diretor();
+  ok3.nome = "Paulo";
+  ok3.idade = 57;
+  ok3.peso = 80;
+  ok3.cpf = "112.111.111-11";
+  ok3.salario = 10000;
+  ok3.assinatura = "teste1";
 
+  Time vasco = Time("nome", "1111", 10000, ok3, ok2);
+  vasco.executarTreino();
 }
-
-
-class Time
-{
-  String _nome, _cnpj;
-  double _fundoMonetario;
-  var _atletas = List(); //olhar se não tem um modo mais específico para escrever lista + testar pra ver se tá dando pra colocar os objetos
-  Diretor _diretor;
-  Tecnico _tecnico;
-  
-  Time(this._nome,this._cnpj,fundoMonetario,atletas(atleta),diretor(diretor),this._tecnico);
-  
-  get nome=> this._nome;
-  get cnpj => this._cnpj;
-  set fundoMonetario(double dinheiro) => this._fundoMonetario = dinheiro;
-  get fundoMonetario => this._fundoMonetario;
-  set atletas(Atleta atleta) =>this._atletas.add(atleta);
-  get atletas => this._atletas;
-  set diretor(Diretor diretor) => this._diretor = diretor;
-  get diretor => this._diretor;
-  set tecnico(Tecnico tecnico) => this._tecnico = tecnico;
-  get tecnico => this._tecnico;
-  
-    void contratarAtleta(Atleta atleta,double valorAtleta,Diretor diretor)
-  {
-    //validar com a assinatura do diretor
-     fundoMonetario -= valorAtleta;
-     atletas(atleta);
-     print("O ${atleta.nome} foi contratado por um valor de $valorAtleta");
-  }
-  
-  
-   void venderAtleta(String cpf,double valorVenda,Diretor diretor)
-  {
-    //validar com a assinatura do diretor
-    fundoMonetario += valorVenda;
-    this._atletas.removeWhere((item)=> cpf == item.nome); //tem que testar, talvez o item.nome não funcione  
-    print("O ${atletas.nome} foi vendido por um valor de $valorVenda");
-  }
-
-  void pagarAtleta(String cpf, double valor,Diretor diretor)
-  {
-    //validar com assinatura do diretor (ideia --> o atleta já foi pago?)
-    fundoMonetario -= valor;
-    this._atletas.forEach(); //em aprendizado ainda, vendo tbm se tem uma maneira melhor de fazer 
-    
-  }
-
-
-
-   void planejarTreino(String nomeTreino,Tecnico tecnico)
-  {
-    //validar o planjamento com a assinatura do tecnico
-    print("O técnico $nome está planejando um treino de $nomeTreino");
-  }
-  
-  
-  void madarTreino(Atleta atletas,Tecnico tecnico)
-  {
-    //validar o treino com a assinatura do tecnico
-    //fazer o envio para o atleta | testar se a list tá funcionando e como atribuir o treino para todos os atletas e(ou) só alguns atletas
-    print("O técnico $nome está executando o treino para o(s) $atletas");
-   
-  }
-  
-}
-
-
 
 abstract class Pessoa {
   String _nome;
@@ -80,6 +43,7 @@ abstract class Pessoa {
   double _peso;
   String _cpf;
   double _salario;
+  String _assinatura;
 
   set nome(String nome) => this._nome = nome;
   get nome => this._nome;
@@ -93,47 +57,109 @@ abstract class Pessoa {
   set cpf(String cpf) => this._cpf = cpf;
   get cpf => this._cpf;
 
+  set assinatura(String assinatura) =>
+      this._assinatura = assinatura.hashCode.toString();
+  get assinatura => this._assinatura;
+
   set salario(double dinheiro) => this._salario = dinheiro;
   get salario => this._salario;
 }
 
+class Time {
+  String _nome, _cnpj;
+  double _fundoMonetario;
+  Diretor _diretor;
+  Tecnico _tecnico;
 
+  Time(this._nome, this._cnpj, fundoMonetario, this._diretor, this._tecnico);
 
+  get nome => this._nome;
+  get cnpj => this._cnpj;
+  set fundoMonetario(double dinheiro) => this._fundoMonetario = dinheiro;
+  get fundoMonetario => this._fundoMonetario;
+  set diretor(Diretor diretor) => this._diretor = diretor;
+  get diretor => this._diretor;
+  set tecnico(Tecnico tecnico) => this._tecnico = tecnico;
+  get tecnico => this._tecnico;
 
-class Diretor extends Pessoa{
-  
-  final String _assinatura = "teste teste".hashCode.toString(); //trocar para GUID, usar API uuid?
-  get assinatura => this._assinatura;
+  /* void contratarAtleta(Atleta atleta, double valorAtleta, Diretor diretor) {
+    //validar com a assinatura do diretor
+    fundoMonetario -= valorAtleta;
+    atletas(atleta);
+    print("O ${atleta.nome} foi contratado por um valor de $valorAtleta");
+  } 
 
+  void venderAtleta(String cpf, double valorVenda, Diretor diretor) {
+    //validar com a assinatura do diretor
+    fundoMonetario += valorVenda;
+    this._atletas.removeWhere((item) =>
+        cpf == item.nome); //tem que testar, talvez o item.nome não funcione
+    print("O ${atletas.nome} foi vendido por um valor de $valorVenda");
+  }
 
-  String assinar()
-  {
-    return assinatura;
+  void pagarAtleta(String cpf, double valor, Diretor diretor) {
+    //validar com assinatura do diretor (ideia --> o atleta já foi pago?)
+    fundoMonetario -= valor;
+    this._atletas .forEach(); //em aprendizado ainda, vendo tbm se tem uma maneira melhor de fazer
+  } */
+
+  PlanejamentoTreino _obterTreino() {
+    return this._tecnico.treino;
+  }
+
+  void executarTreino() {
+    if (_obterTreino().instrucoes != "" &&
+        this._tecnico.assinatura == _obterTreino().assinatura) {
+      return print(
+          "O técnico ${this._tecnico.nome} ira aplicar um treino na data de ${_obterTreino().data},as instruções seguem abaixo \n${_obterTreino().instrucoes}");
+    } else {
+      return print("Treino negado");
+    }
   }
 }
 
+class Tecnico extends Pessoa {
+  PlanejamentoTreino _treino;
 
+  get treino => this._treino;
 
-
-class Tecnico extends Pessoa
-{
- 
-    final String _assinatura = "teste teste".hashCode.toString(); //trocar para GUID, usar API uuid?
-  get assinatura => this._assinatura;
-
-
-  String assinar()
-  {
+  String assinar() {
     return assinatura;
   }
-  
+
+  void condicaoAtleta(Atleta atleta) {
+    print("O ${atleta.nome} está com ${atleta.peso}");
+  }
+
+  void planejarTreino(String instrucoes, DateTime data) {
+    PlanejamentoTreino treino = PlanejamentoTreino();
+    treino.instrucoes = instrucoes;
+    treino.data = data;
+    treino.assinatura = this.assinar();
+    this._treino = treino;
+  }
 }
 
+class PlanejamentoTreino {
+  String _instrucoes;
+  DateTime _data;
+  String _assinaturaTecnico;
 
+  set instrucoes(String instrucoes) => this._instrucoes = instrucoes;
+  get instrucoes => this._instrucoes;
 
+  set data(DateTime data) => this._data = data;
+  get data => this._data;
 
+  set assinatura(String assinatura) => this._assinaturaTecnico = assinatura;
+  get assinatura => this._assinaturaTecnico;
+}
 
-
+class Diretor extends Pessoa {
+  String assinar() {
+    return assinatura;
+  }
+}
 
 class Atleta extends Pessoa {
   String _modalidade;
@@ -142,12 +168,12 @@ class Atleta extends Pessoa {
   get tipo => this._modalidade;
 
   void gastarSalario(double dinheiro) {
-    if(dinheiro > salario){
-      print("$nome não pode gastar essa quantia, o salário atual é de $salario reais");
-    }
-    else{
-    this._salario -= dinheiro;
-    print("O atleta $nome gastou $dinheiro e agora tem $salario reais");
+    if (dinheiro > salario) {
+      print(
+          "$nome não pode gastar essa quantia, o salário atual é de $salario reais");
+    } else {
+      this._salario -= dinheiro;
+      print("O atleta $nome gastou $dinheiro e agora tem $salario reais");
     }
   }
 
@@ -189,6 +215,6 @@ class Atleta extends Pessoa {
 
   void listarDados() {
     return print(
-        "Nome:$nome \nIdade:$idade \nPeso:$peso \nCpf:$cpf \nModalidade:$modalidade \nSalário:$salario");
+        "Nome:$nome \nIdade:$idade \nPeso:$peso \nCpf:$cpf \nTipo:$tipo \nSalário:$salario \nAssinatura:$assinatura");
   }
 }
